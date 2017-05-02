49fe349dbc4aa1f30c012e66a5ceab68810c51b0
Alexandre Fonseca Faria <alexandreffaria@me.com>
2017-05-02 15:25:46 -0300

    First trys on the 'Exceeding features'

--- js/script.js
+++ js/script.js
@@ -7,24 +7,30 @@ var quotes = [
     quote: "We probably are, in a simulation",
     source: "Elon Musk",
     citation: "Code conference",
-    year: 2016
+    year: 2016,
+    tags: ['Universe', 'Matrix', 'Mindblowing']
   },
   {
     quote: "Obviously this feels real, but it seams unlikely to be real.",
     source: "Elon Musk",
     citation: "Vanity Fair",
-    year: 2014
+    year: 2014,
+    tags: ['Universe', 'Matrix', 'Mindblowing']
   },
   {
-    quote: "Our Sun is a second- or third-generation star. All of the rocky and metallic material we stand on, the iron in our blood, the calcium in our teeth, the carbon in our genes were produced billions of years ago in the interior of a red giant star. We are made of star-stuff.",
+    quote: "Our Sun is a second- or third-generation star. All of the rocky and metallic material we stand on, the iron in our blood, the calcium in our teeth, the carbon in our genes were produced billions of years ago in the interior of a red giant star. <strong><em>We are made of star-stuff.</em></strong>",
     source: "Carl Sagan",
-    citation: "The Cosmic Connection: An Extraterrestrial Perspective"
-    year: 1973
+    citation: "The Cosmic Connection: An Extraterrestrial Perspective",
+    year: 1973,
+    tags: ['Cosmos', 'Mindblowing', 'Aliens', 'Universe']
   }
 ];
 
+function getRandomNumber(upper) {
+  return Math.floor(Math.random()*upper);
+}
 function getRandomQuote() {
-  var i = Math.floor(Math.random()*quotes.length);
+  var i = getRandomNumber(quotes.length);
   return quotes[i];
 
 }
@@ -38,8 +44,28 @@ function printQuote() {
     if(key === 'source') html += '<p class="source">' + quoteObj[key];
     if(key === 'citation') html += '<span class="citation">' + quoteObj[key] + '</span>';
     if(key === 'year') html += '<span class="year">' + quoteObj[key] + '</span>';
+    if(key === 'tags'){
+      html += '<span class="tags">';
+      for(var i = 0 ; i < key.length ; i++){
+         var tag = key[i].tags;
+         html += tag + ' ,';
+
+        }
+      html += '</span>';
+      }
   }
   html += '</p>';
   document.getElementById('quote-box').innerHTML = html;
+  changeBackground();
+}
+
+
+ // Grabs the container div and changes it to a randomized RGB color.
+function changeBackground(){
+  var r = getRandomNumber(255);
+  var g = getRandomNumber(255);
+  var b = getRandomNumber(255);
+  var color = '(' + r + ',' + g + ',' +  b + ')';
+  document.getElementsByClassName('container').style.color = 'black';
 
 }
